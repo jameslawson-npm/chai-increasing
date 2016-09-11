ae410bb07edb21a3685aff011dac1c2577e86d66
James Lawson <jameslawson@users.noreply.github.com>
2016-09-11 12:17:19 +0100

    Improve error messages

--- matcher/decreasing.js
+++ matcher/decreasing.js
@@ -10,13 +10,22 @@ function sorted(list, isStrict) {
   return list.skip(1).zipWith(comparitor, list).every(identity);
 }
 
+var MSG_MONO = {
+  ERR_MSG: 'expected #{this} to be monotonicaly decreasing',
+  NOT_MSG: 'expected #{this} to not be monotonically decreasing'
+};
+var MSG_STRICT = {
+  ERR_MSG: 'expected #{this} to be strictly decreasing',
+  NOT_MSG: 'expected #{this} to not be strictly decreasing'
+};
+
 function makeDecreasing(chai, utils) {
   function decreasing() {
     var list = fromJS(this._obj);
-    var passes  = sorted(list, utils.flag(this, STRICTLY_FLAG));
-    var ERR_MSG = 'expected #{this} to monotonicaly decreasing';
-    var NOT_MSG = 'expected #{this} to not be monotonically decreasing';
-    this.assert(passes, ERR_MSG, NOT_MSG);
+    var isStrict = utils.flag(this, STRICTLY_FLAG) === true;
+    var passes  = sorted(list, isStrict);
+    var MSG = isStrict ? MSG_STRICT : MSG_MONO;
+    this.assert(passes, MSG.ERR_MSG, MSG.NOT_MSG);
   }
   return decreasing;
 }
--- matcher/increasing.js
+++ matcher/increasing.js
@@ -10,13 +10,22 @@ function sorted(list, isStrict) {
   return list.skip(1).zipWith(comparitor, list).every(identity);
 }
 
+var MSG_MONO = {
+  ERR_MSG: 'expected #{this} to be monotonicaly increasing',
+  NOT_MSG: 'expected #{this} to not be monotonically increasing'
+};
+var MSG_STRICT = {
+  ERR_MSG: 'expected #{this} to be strictly increasing',
+  NOT_MSG: 'expected #{this} to not be strictly increasing'
+};
+
 function makeIncreasing(chai, utils) {
   function increasing() {
     var list = fromJS(this._obj);
-    var passes  = sorted(list, utils.flag(this, STRICTLY_FLAG));
-    var ERR_MSG = 'expected #{this} to monotonicaly increasing';
-    var NOT_MSG = 'expected #{this} to not be monotonically increasing';
-    this.assert(passes, ERR_MSG, NOT_MSG);
+    var isStrict = utils.flag(this, STRICTLY_FLAG) === true;
+    var passes  = sorted(list, isStrict);
+    var MSG = isStrict ? MSG_STRICT : MSG_MONO;
+    this.assert(passes, MSG.ERR_MSG, MSG.NOT_MSG);
   }
   return increasing;
 }
